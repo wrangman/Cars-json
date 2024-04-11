58cabb7e839da2c05910a43b5a91af39fcee0671
wrangman <wrangman@gmail.com>
2024-04-11 11:14:17 +0200

    initial commit

---
+++ Cars.py
@@ -0,0 +1,41 @@
+'''
+CARS.PY: Bilklass
+
+__author__  = "Johan Wrangö"
+__version__ = "1.0.3"
+__email__   = "johan.wrango@ntig.se"
+'''
+
+import json
+
+class MyCars():
+    def __init__(self):
+        self.cars = []
+
+    def list_cars(self):
+        print("BILLISTA:")
+        for i, car in enumerate(self.cars):
+            print(f"{i+1}. {car['make']} {car['model']}")
+              
+    def add_car(self, make, model):
+        self.cars.append({ "make": make, "model": model })
+        
+    def remove_car(self, index):
+        del self.cars[index]
+        
+    def sort_cars(self):                                    # Sort the list of cars in place by make and model
+        self.cars.sort(key=lambda car: (car["make"], car["model"]))
+        
+    def save_to_file(self, filename):                       # Saves to JSON
+        with open(filename, 'w') as f:
+            json_string = json.dumps(self.cars)
+            f.write(json_string)
+        
+    def load_from_file(self, filename):                     # Loads the cars from a JSON file
+        try:
+            with open(filename, "r") as file:
+                return json.load(file)
+                
+        except FileNotFoundError:
+            return False
+    
---
+++ car_list.py
@@ -0,0 +1,83 @@
+'''
+CAR_LIST.PY: Dina favoritbilar :)
+
+__author__  = "Johan Wrangö"
+__version__ = "1.0.3"
+__email__   = "johan.wrango@ntig.se"
+'''
+
+import os
+import time
+from Cars import MyCars
+from bcolors import bcolors
+
+car = MyCars()
+auto_sort = False
+
+stored_cars = car.load_from_file("cars.json")  #Load stored cars from file - the function is in Cars.py
+
+if stored_cars:                                #If there are stored cars add them
+    car.cars = stored_cars    
+
+os.system('cls')
+
+print(bcolors.YELLOW + """
+FAVORITBILAR ===================================================            
+- Lägg till bil genom att skriva märke och modell med mellanslag
+- Ta bort genom att skriva respektive nummer
+- Aktivera/deaktivera sorteringsfunktion: "/sort"
+- Spara & avsluta genom att skriva "/q"
+================================================================""" + bcolors.CYAN)
+
+if len(car.cars) > 0: 
+    car.list_cars() 
+    
+else: 
+    print(bcolors.CYAN + "(Inga sparade bilar hittades)")
+
+while True:
+    print(bcolors.YELLOW + "-" * 64)
+    entry = input(bcolors.DEFAULT + "Lägg till bil (märke & modell): " + bcolors.CYAN)
+
+    if entry.upper() == "/Q" or entry.upper() == "/QUIT": 
+        print(bcolors.DEFAULT + "Sparar din billista...")
+        time.sleep(1)
+        car.save_to_file("cars.json")
+        exit()
+        
+    if entry.upper() == "/S" or entry.upper() == "/SORT":
+        os.system('cls')
+        car.sort_cars()
+        car.list_cars()
+        if auto_sort:
+            auto_sort = False
+        else:
+            auto_sort = True
+        continue
+    
+    if entry.isdigit():
+        total_cars = len(car.cars)
+        
+        if int(entry) >= 1 and int(entry) < total_cars + 1:
+            car.remove_car(int(entry)-1)
+        
+        else:
+            if total_cars > 0:                           #if no cars - display nothing
+                print(bcolors.RED + f"Bilen finns inte - skriv ett nummer mellan 1 och {total_cars}")
+                time.sleep(1)
+    
+    else:
+        if not " " in entry:
+            print(bcolors.RED + "Felaktig inmatning - skriv märke & modell")
+            time.sleep(1)
+    
+        else:                                           # add new car                                   
+            make, model = entry.split(" ",1)
+            car.add_car(make, model)
+        
+    os.system('cls')
+    print(bcolors.CYAN)
+    if auto_sort: car.sort_cars()                   # if you sort the cars they will automatically sort until you disable with /s
+    car.list_cars()
+    
+    
